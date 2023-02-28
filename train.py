dba2cfb784fc30631fc1ab6b8d9af9590308cc4b
michaelzhang <48890588+mikezhang25@users.noreply.github.com>
2023-02-27 17:25:07 -0800

    Update train.py

--- train.py
+++ train.py
@@ -7,6 +7,8 @@ from datasets import load_dataset
 
 class Pretraining:
     def __init__(self) -> None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"Setting up Pretraining on {device}")
         self.model = BigBirdPegasusForQuestionAnswering.from_pretrained(
             "google/bigbird-pegasus-large-arxiv")
         self.tokenizer = AutoTokenizer.from_pretrained(
@@ -20,7 +22,7 @@ class Pretraining:
         self.train_dataset = load_dataset(
             "allenai/multi_lexsum", name="v20220616", split="train")
         # only train on the short (1 paragraph) summaries for now
-        self.train_dataset.remove_columns(
+        self.train_dataset = self.train_dataset.remove_columns(
             ["id", "summary/long", "summary/tiny"])
         print(self.train_dataset.column_names)
         # wiki = wiki.remove_columns([col for col in wiki.column_names if col != "text"])  # only keep the 'text' column
