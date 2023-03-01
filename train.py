172b498567ce7607156c22049097b8a39f447231
michaelzhang <48890588+mikezhang25@users.noreply.github.com>
2023-03-01 13:39:32 -0800

    Adding evaluation method

--- train.py
+++ train.py
@@ -6,10 +6,13 @@ import numpy as np
 from nltk.tokenize import sent_tokenize
 from datasets import load_dataset
 import evaluate
+from evaluate import evaluator
+import argparse
+
 
 class LegalModel:
     def __init__(self, checkpoint) -> None:
-        #TODO: Add more modularity to FineTuning Class & Make Legal Model Class 
+        # TODO: Add more modularity to FineTuning Class & Make Legal Model Class
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         print(f"Setting up finetuning on {self.device}")
         self.model = BigBirdPegasusForConditionalGeneration.from_pretrained(
@@ -33,25 +36,23 @@ class LegalModel:
         self.MAX_LENGTH = 512
         self.MAX_TARGET_SIZE = 512
 
-
     def load_arxiv_dataset(self):
         self.train = load_dataset(
             "ccdv/arxiv-summarization", split="train[:5%]")
         self.test = load_dataset(
             "ccdv/arxiv-summarization", split="validation[:5%]")
-        
+
     def load_multilex_dataset(self):
         self.train = load_dataset(
             "allenai/multi_lexsum", name="v20220616", split="train[:1%]")
         self.test = load_dataset(
             "allenai/multi_lexsum", name="v20220616", split="validation[:1%]")
-    
-    def load_billsum_dataset(self): 
-        self.train = load_dataset(
-            "billsum", split="train[:36]")
-        self.test = load_dataset(
-            "billsum", split="test[:36]")
 
+    def load_billsum_dataset(self):
+        self.train = load_dataset(
+            "billsum", split="train")
+        self.test = load_dataset(
+            "billsum", split="test")
 
     def compute_metrics(self, eval_pred):
         predictions, labels = eval_pred
@@ -83,9 +84,7 @@ class LegalModel:
             self.billsum_preprocess_function
         )
 
-        print(len(tokenized_train))
-
-        batch_size = 8
+        batch_size = 4 # breaks when we hit 8
         num_train_epochs = 3
         # Show the training loss with every epoch
         logging_steps = len(tokenized_train) // batch_size
@@ -142,30 +141,45 @@ class LegalModel:
         model_inputs["labels"] = labels["input_ids"]
         return model_inputs
 
-    def lexsum_preprocess_function(self, examples): 
+    def lexsum_preprocess_function(self, examples):
         sources = examples["sources"]
         concat_sources = ""
-        for s in sources: 
+        for s in sources:
             concat_sources += str(s)
         n = 4096
         chunks = [str[i:i+n] for i in range(0, len(str), n)]
 
-        model_inputs = self.tokenizer(str(examples["sources"]), max_length=self.MAX_LENGTH, truncation=True)
-        labels = self.tokenizer(text_target=examples["summary/long"], max_length=self.MAX_TARGET_SIZE, truncation=True)
+        model_inputs = self.tokenizer(
+            str(examples["sources"]), max_length=self.MAX_LENGTH, truncation=True)
+        labels = self.tokenizer(
+            text_target=examples["summary/long"], max_length=self.MAX_TARGET_SIZE, truncation=True)
         model_inputs["labels"] = labels["input_ids"]
 
         return model_inputs
 
-    def billsum_preprocess_function(self, examples): 
-        model_inputs = self.tokenizer(examples["text"], max_length=self.MAX_LENGTH, truncation=True)
-        labels = self.tokenizer(text_target=examples["summary"], max_length=self.MAX_TARGET_SIZE, truncation=True)
+    def billsum_preprocess_function(self, examples):
+        model_inputs = self.tokenizer(
+            examples["text"], max_length=self.MAX_LENGTH, truncation=True)
+        labels = self.tokenizer(
+            text_target=examples["summary"], max_length=self.MAX_TARGET_SIZE, truncation=True)
         model_inputs["labels"] = labels["input_ids"]
         return model_inputs
+
+    def evaluate_model(self):
+        # data = load_dataset("imdb", split="test").shuffle(seed=42).select(range(1000))
+        print(self.test.column_names)
+        # self.test = self.test.rename_column("article", "input_column")
+        # self.test = self.test.rename_column("abstract", "label_column")
+        # print(self.test.column_names)
+        task_evaluator = evaluator("summarization")
+        results = task_evaluator.compute(
+            model_or_pipeline=self.model, tokenizer=self.tokenizer, data=self.test, input_column="text", label_column="summary")
+        return results
 
 
 if __name__ == "__main__":
     #pretrain = Pretraining()
     #tokens = pretrain.tokenizer("Sample text is true")
-    
     legalModel = LegalModel("arxiv_sum")
-    legalModel.train_model()
+    # legalModel.train_model()
+    legalModel.evaluate_model()
