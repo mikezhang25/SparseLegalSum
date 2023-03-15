b1b365755a424701994a284dd29546ce7ea73ae6
michaelzhang <48890588+mikezhang25@users.noreply.github.com>
2023-03-14 21:03:06 -0700

    added file for chunking big bird

---
+++ src/chunkbigbird.py
@@ -0,0 +1,4 @@
+"""
+BigBirdPegasus model that implements the custom chunking mechanism
+Source: https://stackoverflow.com/questions/70814490/uploading-models-with-custom-forward-functions-to-the-huggingface-model-hub
+"""
